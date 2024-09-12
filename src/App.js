13cb95ae4c5e5ff0c73990923f932bfd96a99262
Abraham12611 <abraham.dahunsi@gmail.com>
2024-09-12 09:19:42 +0100

    Feat:ErrorBoundary --Set in Route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
+// App.js
 import React from 'react';
 import './index.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';  
 import HomePage from './pages/HomePage';   
 import SignUp from './pages/SignUp';    
+import ErrorBoundary from './components/ErrorBoundary'; // Import ErrorBoundary
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />    {/* Landing page route */}
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/dashboard" element={<HomePage />} /> {/* Dashboard/Home Page route */}
-      </Routes>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />    {/* Landing page route */}
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<HomePage />} /> {/* Dashboard/Home Page route */}
+        </Routes>
+      </ErrorBoundary>
     </Router>
   );
 };
