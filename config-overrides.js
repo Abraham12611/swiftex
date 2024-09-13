29e95d06090562a45c0f8619aa4cbca3f0a83a1e
Abraham12611 <abraham.dahunsi@gmail.com>
2024-09-13 10:28:21 +0100

    fix: #08

--- config-overrides.js
+++ config-overrides.js
@@ -4,7 +4,20 @@ module.exports = function override(config, env) {
   config.resolve.fallback = {
     ...config.resolve.fallback,
     "crypto": require.resolve("crypto-browserify"),
-    "url": require.resolve("url")
+    "url": require.resolve("url"),
+    "stream": require.resolve("stream-browserify"),
+    "vm": require.resolve("vm-browserify"),
+    "process": require.resolve("process/browser") // Ensure this line is here
   };
+
+  config.resolve.extensions = [...config.resolve.extensions, '.js', '.jsx', '.json', '.mjs']; // Add .mjs extension
+
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer']
+    })
+  ]);
+
   return config;
 };
